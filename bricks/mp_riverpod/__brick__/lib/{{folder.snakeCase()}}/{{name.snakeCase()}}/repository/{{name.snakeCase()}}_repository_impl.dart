173e216a756b4804075837d08f6f3b66a0a7bda5
Emil Apelgren <emilapelgren@outlook.com>
2023-10-05 11:59:00 +0100

    added dio

--- bricks/mp_riverpod/__brick__/lib/{{folder.snakeCase()}}/{{name.snakeCase()}}/repository/{{name.snakeCase()}}_repository_impl.dart
+++ bricks/mp_riverpod/__brick__/lib/{{folder.snakeCase()}}/{{name.snakeCase()}}/repository/{{name.snakeCase()}}_repository_impl.dart
@@ -1,10 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '{{name.snakeCase()}}_repository.dart';
 
 class {{name.pascalCase()}}RepositoryImpl extends {{name.pascalCase()}}Repository {
+  final Dio dio;
+  {name.pascalCase()}}RepositoryImpl(this.dio);
+
   // TODO add your methods here
 }
 
 final {{name.camelCase()}}RepositoryProvider = Provider<{{name.pascalCase()}}Repository>((ref) {
-  return {{name.pascalCase()}}RepositoryImpl();
+  final dio = ref.watch(dioProvider);
+  return {{name.pascalCase()}}RepositoryImpl(dio);
 });
